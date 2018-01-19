175fa8dd3256565338d87cbc1a5af405bbc563f1
Mike Fix <mrfix84@gmail.com>
2018-01-18 19:52:30 -0800

    This one is gonna work

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-NOW_URL=$(now -e NODE_ENV=production --public --team dawn --token $NOW_TOKEN)
+NOW_URL=$(now -e NODE_ENV=production --public --no-clipboard --team=dawn --token "$NOW_TOKEN")
 
 curl -X POST \
   -H 'Content-type: application/json' \
   --data "{\"text\": \":rocket: New Carbon PR Deployment: $NOW_URL\"}" \
-  $DAWN_SLACK_WEBHOOK
+  "$DAWN_SLACK_WEBHOOK"
