57b439e2fdaec03261010b205ada7b8601d1a101
Mike Fix <mrfix84@gmail.com>
2018-01-18 20:50:18 -0800

    Better logging

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 NOW_URL=$(now -e NODE_ENV=production --public --no-clipboard --team=dawn --token "$NOW_TOKEN")
 
+echo "$TRAVIS_COMMIT_MESSAGE"
 curl -X POST \
   -H 'Content-type: application/json' \
-  --data "{\"text\": \":rocket: New Carbon PR Deployment: $NOW_URL\"}" \
+  --data "{\"text\": \":rocket: New Carbon PR Deployment for <https://github.com/dawnlabs/carbon/pulls/$TRAVIS_PULL_REQUEST|PR #$TRAVIS_PULL_REQUEST>: $NOW_URL\"}" \
   "$DAWN_SLACK_WEBHOOK"
